2333676144c0d8def58c207014acf63ba9586b29
AzOrino <azokikru@gmail.com>
2025-03-09 18:08:33 +0530

    2

--- face reconition attendance.py
+++ face reconition attendance.py
@@ -1,1 +1,15 @@
 import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox as mess
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
+
+
+import tkinter.simpledialog as tsd
+import cv2,os
+import csv
+import numpy as np
+from PIL import Image
+import pandas as pd
+import datetime
+import time
